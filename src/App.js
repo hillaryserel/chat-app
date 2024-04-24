2c59bd48c62d41a73bc76468bc6d56558a5230df
Hillary Serel <Serelhillary00@gmail.com>
2024-04-25 02:26:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ChatFeed } from './components/ChatFeed';
+import './App.css';
+const App= () => {
+    return (
+        <ChatEngine
+        height="100vh"
+        projectID="853926aa-6703-4332-8e08-8d1558dc2e1c"
+        userName="larrythecoder"
+        userSecret="2020"
+        renderChatFeed={(chatAppProps) => <ChatFeed{...chatAppProps} />}
+        />
+    );
 }
 
 export default App;
